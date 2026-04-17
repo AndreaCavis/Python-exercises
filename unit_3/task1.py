18381d92be0785209d1cb76d4d378c67263bc1fd
Andrea Canestrari <andrea_4991@hotmail.it>
2026-04-17 19:52:23 +0100

    Correct unit 3 ex 1 solution

--- unit_3/task1.py
+++ unit_3/task1.py
@@ -1,29 +1,35 @@
 def solution(numbers):
     result = []
     n = len(numbers)
-    obstacle_indices = find_obstacles(numbers)
-    j = 0
 
     for i in range(n):
-        if j < len(obstacle_indices):
-            if i < obstacle_indices[j]:
-                result.append(obstacle_indices[j])
-            else:
-                result.append(-1)
-                if j < len(obstacle_indices):
-                    j += 1
+        if numbers[i] < 0:
+            result.append(-1)
+            continue
+
+        end_range = min(i + 1 + numbers[i], n)
+        obstacle = None
+
+        for j in range(i + 1, end_range):
+            if numbers[j] < 0:
+                obstacle = j
+                break
+
+        if obstacle is not None:
+            result.append(obstacle)
         else:
             result.append(numbers[i])
-            
+
+               
     return result
     
 
-def find_obstacles(numbers):
-    obstacles = []
-    for i in range(len(numbers)):
-        if numbers[i] < 0:
-            obstacles.append(i)
-    return obstacles
+# def find_obstacles(numbers):
+#     obstacles = []
+#     for i in range(len(numbers)):
+#         if numbers[i] < 0:
+#             obstacles.append(i)
+#     return obstacles
 
 
 # output [2, 2, -1, 1, 2].
