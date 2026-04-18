f543183f31173e9c3279fd273f839de43eb3666e
Andrea Canestrari <andrea_4991@hotmail.it>
2026-04-18 16:02:01 +0100

    Unit 3 task 2 solved

---
+++ unit_3/task2.py
@@ -0,0 +1,30 @@
+def evaluatePath(numbers):
+    n = len(numbers)
+    position, moves = 0, 0
+    total_reversals = 2
+    opposite_direction = False
+    
+    while total_reversals > 0:
+        value = numbers[position]
+        # if 0, game ends
+        if value == 0:
+            return (position, moves)
+        
+        if opposite_direction:
+            value = -value
+        
+        next_position = position + value
+        
+        if next_position < 0 or next_position >= n:
+            opposite_direction = not opposite_direction
+            total_reversals -= 1
+            # second touch of list border, game ends
+            if total_reversals == 0:
+                return (position, moves)
+            continue
+        else:
+            position += value
+        
+        moves += 1
+
+    return (position, moves)
