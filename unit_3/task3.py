c58b9e550cea3393242e058da3602af5ef08fa0f
Andrea Canestrari <andrea_4991@hotmail.it>
2026-04-19 19:13:01 +0100

    First functional version of solution to task 3 unit 3

--- unit_3/task3.py
+++ unit_3/task3.py
@@ -4,30 +4,27 @@ def solution(board, obstacle):
     
     for i in range(n):
         move = 0
+        position = i
         
-        if board[i] == obstacle:
+        if board[position] == obstacle:
             moves.append(-1)
             continue
-        
-        while i < n:
-            next_position = i + board[i]
+        # THIS WORKS
+        while position < n:
+            next_position = position + board[position]
             
-            if next_position == obstacle:
-                moves.append(-1)
-                break
-                # i += board[i]
-                # continue
-            
-            if next_position > n:
+            if next_position >= n:
                 moves.append(move + 1)
-                i += board[i]
+                position += board[position]
                 continue
             else:
-                i += board[i]
+                if board[next_position] == obstacle:
+                    moves.append(-1)
+                    break
+                position += board[position]
+                move += 1
 
-            move += 1
     return moves
-
 
 #  output: [3, -1, 3, 1, 2, 2, 1]
 print(solution([5, 3, 2, 6, 2, 1, 7], 3))
