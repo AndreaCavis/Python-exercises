7cb28d225bddcb23e95413d25d1dec6cbbdc4c94
Andrea Canestrari <andrea_4991@hotmail.it>
2026-04-25 00:15:54 +0100

    unit 4 lesson code

--- unit_4/lesson.py
+++ unit_4/lesson.py
@@ -1,13 +1,20 @@
 # position-value notation
 def solution(num1, num2):
-    # two pinters initialised to the end of each string (len() - 1 due to 0 indexing)
+    # two pointers initialised to the end of each string (len() - 1 due to 0 indexing)
     i, j = len(num1) - 1, len(num2) - 1
     # carry is needed to hold carryovers from each addition
     carry, res = 0, []
 
+    while i >= 0 or j >= 0 or carry > 0:
+        n1 = int(num1[i]) if i >= 0 else 0
+        n2 = int(num2[j]) if j >= 0 else 0
+        total = n1 + n2 + carry
+        carry = total // 10
+        res.append(str(total % 10))
+        i -= 1
+        j -= 1
 
-
-    return res
+    return "".join(res[::-1])
 
 
 
