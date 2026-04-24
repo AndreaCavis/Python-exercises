73634034c6c508cf3e8507bba23763127b414426
Andrea Canestrari <andrea_4991@hotmail.it>
2026-04-25 00:06:31 +0100

    update

---
+++ unit_4/lesson.py
@@ -0,0 +1,15 @@
+# position-value notation
+def solution(num1, num2):
+    # two pinters initialised to the end of each string (len() - 1 due to 0 indexing)
+    i, j = len(num1) - 1, len(num2) - 1
+    # carry is needed to hold carryovers from each addition
+    carry, res = 0, []
+
+
+
+    return res
+
+
+
+
+print(solution("111111111111111", "222222222222222"))
