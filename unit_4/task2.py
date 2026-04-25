504be84d09063d1c8a6dc1230f67d00a18e22561
Andrea Canestrari <andrea_4991@hotmail.it>
2026-04-25 12:54:50 +0100

    Completed unit4 ex2

--- unit_4/task2.py
+++ unit_4/task2.py
@@ -1,18 +1,23 @@
 def solution(num1, num2):
-    i, j, carry = 0, 0, 0
-    num3 = []
+    i, j, carry = len(num1) - 1, len(num2) - 1, 0
+    res = []
     
-    while i < len(num1) or j < len(num1) or carry > 0:
+    while i >= 0 or j >= 0 or carry > 0:
         n1 = int(num1[i]) if i >= 0 else 0
         n2 = int(num2[j]) if j >= 0 else 0
-        if n2 > n1:
-            # TODO: this doesn't cover 100 - 1, 1000 - 1, etc
-            difference = int(num1[i:i+2]) - n2
+        
+        if (n1 - carry) >= n2:
+            difference = n1 - carry - n2
+            carry = 0
         else:
-            difference = n1 - n2
-        num3.append(str(difference))
-        i += 1
-        j += 1
-    
-    
-    return "".join(num3[::-1])
+            difference = (10 + n1 - carry) - n2
+            carry = 1
+            
+        res.append(str(difference))
+        i -= 1
+        j -= 1
+        
+        clean_res = "".join(res[::-1])
+        num3 = clean_res.lstrip("0")
+
+    return num3 if num3 else "0"
