3a77f769b8b18f105d38fff1d7eead213a0fac8d
Andrea Canestrari <andrea_4991@hotmail.it>
2026-04-28 15:27:24 +0100

    Created text and function for 2 pointers same dir

--- practice_code/two_pointers/same_direction.py
+++ practice_code/two_pointers/same_direction.py
@@ -1,15 +1,28 @@
-# this comment contains the the example problem statement for the two pointers same direction problem
+'''
+REMOVE DUPLICATES (SAME DIRECTION) - The goal is to remove duplicates in a SORTED array in place, directly modifying the input array
+                                     such that all unique elements appear at the front of the array.
 
-def two_pointers_same_direction(arr, target):
-    left, right = 0, len(arr) - 1
-    while left < right:
-        current_sum = arr[left] + arr[right]
-        if current_sum == target:
-            return [left, right]
-        elif current_sum < target:
-            left += 1
+solution = the key here is the same, the array is SORTED. 
+            SLOW and FAST start next to each other. If num[slow] == num[fast] (i.e. duplicates), FAST moves forward.
+            if num[slow] != num[fast], SLOW moves forward and the value at FAST is copied to SLOW.
+            This cycle repeats unutil FAST is done traversing the array. The result will be that all values up to SLOW are unique.
+'''
+
+def remove_duplicates(arr):
+    if not arr:
+        return 0
+    
+    slow, fast = 0, 1
+
+    while fast < len(arr):
+        if arr[slow] == arr[fast]:
+            fast += 1
         else:
-            right -= 1
-    return [-1, -1]
+            slow += 1
+            arr[slow] = arr[fast]
+
+    return arr[:slow + 1]
 
 
+print(remove_duplicates([1, 1, 2, 2, 3])) # [1,2,3]
+print(remove_duplicates([1, 1, 1, 2, 2, 3, 3, 4, 5]))
