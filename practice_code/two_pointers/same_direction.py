94b379df30be47bfe3e84f754115b30b20292985
Andrea Canestrari <andrea_4991@hotmail.it>
2026-04-28 13:21:53 +0100

    Created branch, testing push on branch

---
+++ practice_code/two_pointers/same_direction.py
@@ -0,0 +1,15 @@
+# this comment contains the the example problem statement for the two pointers same direction problem
+
+def two_pointers_same_direction(arr, target):
+    left, right = 0, len(arr) - 1
+    while left < right:
+        current_sum = arr[left] + arr[right]
+        if current_sum == target:
+            return [left, right]
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+    return [-1, -1]
+
+
