fc9e9e4471d6d2057114f519b25f7a1e1eb48bcf
Andrea Canestrari <andrea_4991@hotmail.it>
2026-04-28 15:06:34 +0100

    2 pointers opposite direction completed

--- practice_code/two_pointers/opposite_direction.py
+++ practice_code/two_pointers/opposite_direction.py
@@ -7,4 +7,22 @@ solution = the key to the solution is that the array is SORTED, meaning that the
            if sum > target, move r left.    if sum < target, move l right.    if sum == target, return the indices.
 '''
 
+def two_pointers_opposite_direction(arr, target):
+    left, right = 0, len(arr) - 1
+    result = []
 
+    while left < right:
+        current_sum = arr[left] + arr[right]
+        if current_sum == target:
+            result.append(left)
+            result.append(right)
+            return result
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+        
+    return [-1, -1]
+
+print(two_pointers_opposite_direction([1, 2, 3, 4, 6], 6)) # [1, 3]
+print(two_pointers_opposite_direction([2, 3, 4, 5, 8, 11, 18], 8)) # [1, 3]
