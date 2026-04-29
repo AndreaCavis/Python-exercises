b7be3f9671a9604ad294a35e703249298f723a5f
Andrea Canestrari <andrea_4991@hotmail.it>
2026-04-29 13:57:21 +0100

    Completed unit1 task1

---
+++ cosmo/coding_practice_technical_interview/unit_1/task1.py
@@ -0,0 +1,23 @@
+def solution(arrayA, arrayB):
+    indexA, indexB = 0, None
+    in_arrayA = True
+    visited_arrayA = set()
+    arrayB_path = []
+    # values inside arrays are 1-based, not 0 based like indices
+    while True:
+        if in_arrayA:
+            indexB = arrayA[indexA] - 1
+            arrayB_path.append(indexB + 1)
+        else:
+            indexA = arrayB[indexB] - 1
+            if indexA not in visited_arrayA:
+                visited_arrayA.add(indexA)
+            else:
+                return arrayB_path[:-1]
+                
+        in_arrayA = not in_arrayA
+
+
+arrayA = [1, 3, 2, 5, 4]
+arrayB = [5, 4, 3, 2, 1]
+print(solution(arrayA, arrayB)) #  [1, 4, 3, 2, 5]
