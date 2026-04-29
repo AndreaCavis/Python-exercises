0d1c078f9f50ac98acbb579a1ddb565d4a596bbb
Andrea Canestrari <andrea_4991@hotmail.it>
2026-04-29 12:20:19 +0100

    Completed lesson function

--- cosmo/coding_practice_technical_interview/unit_1/lesson.py
+++ cosmo/coding_practice_technical_interview/unit_1/lesson.py
@@ -1,6 +1,24 @@
 #TODO: do the practice session either today or tomorrow
 
 def solution(arrayA, arrayB):
+    indexA, indexB, max_value = 0, None, float("-inf")
+    in_arrayA = True
+
+    while True:
+        if in_arrayA:
+            indexB = arrayA[indexA]
+            if arrayB[indexB] > max_value:
+                max_value = arrayB[indexB]
+        else:
+            indexA = arrayB[indexB]
+            if indexA == 0:
+                return max_value
+        in_arrayA = not in_arrayA
+
+    
+
+    
+
     return
 
 
