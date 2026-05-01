606b3ebc05fb008f08c33ffc129ae2c48e33117c
Andrea Canestrari <andrea_4991@hotmail.it>
2026-05-01 22:26:55 +0100

    Completed sliding window function

--- practice_code/sliding_windows/sliding_window_2.py
+++ practice_code/sliding_windows/sliding_window_2.py
@@ -1,3 +1,24 @@
-    # TODO: create sliding window solution for the problems in python alchemist
+'''
+Given an array, find the maximum sum of k consecutive elements.
+'''
 
-# com
+def sliding_window(arr, k):
+    window_sum = 0
+
+    for i in range(k):
+        window_sum += arr[i]
+    
+    max_sum = window_sum
+    n = len(arr)
+
+    for i in range(k, n):
+        window_sum -= arr[i-k]
+        window_sum += arr[i]
+        max_sum = max(max_sum, window_sum)
+    
+    return max_sum
+
+
+arr = [2, 5, 1, 8, 3]
+k = 3
+print(sliding_window(arr, k)) # 14
