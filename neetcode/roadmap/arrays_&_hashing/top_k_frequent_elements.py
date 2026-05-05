31782a04f10b174feeed006cce73313efe4de16c
Andrea Canestrari <andrea_4991@hotmail.it>
2026-05-05 12:28:11 +0100

    Completed top k frequent elements exercise

--- neetcode/roadmap/arrays_&_hashing/top_k_frequent_elements.py
+++ neetcode/roadmap/arrays_&_hashing/top_k_frequent_elements.py
@@ -8,4 +8,23 @@ https://neetcode.io/problems/top-k-elements-in-list/question
 
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        return [0]
+        seen = {}
+        # res = set()
+
+        for i in nums:
+            if i in seen:
+                seen[i] += 1
+            else:
+                seen[i] = 1
+        
+        # sort dictionary by values(represented by item[1]) in descending order to find the most frequent elements
+        sorted_seen = sorted(seen.items(), key=lambda item: item[1], reverse=True)
+        # extract keys sorted by frequency
+        res = [num[0] for num in sorted_seen]
+        # return only the most k frequent
+        return res[:k]
+    
+
+solution = Solution()
+
+print(solution.topKFrequent([1,2,2,3,3,3], 2))
