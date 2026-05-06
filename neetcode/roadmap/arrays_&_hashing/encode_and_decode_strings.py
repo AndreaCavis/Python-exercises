36bc46fc9c2733dd6e2df6ede8fbec85e6e6cb0d
Andrea Canestrari <andrea_4991@hotmail.it>
2026-05-06 16:48:02 +0100

    Separated solution and test_solution for encode and decode

--- neetcode/roadmap/arrays_&_hashing/encode_and_decode_strings.py
+++ neetcode/roadmap/arrays_&_hashing/encode_and_decode_strings.py
@@ -67,24 +67,5 @@ class Solution:
         # the trailing -- creates an empty array value. Return the list without it
         decoded_list = s.split("-_-")
         return decoded_list[:-1]
-
     
-
-
-# -------------------- Test Suite ---------------------------------
-solution_test = Solution()
-
-encode = solution_test.encode(["Hello", "World"])
-encode2 = solution_test.encode([","])
-encode3 = solution_test.encode([""])
-encode4 = solution_test.encode(["0"])
-encode5 = solution_test.encode(["", ""])
-encode6 = solution_test.encode(["Cristo", "Cane"])
-decode = solution_test.decode
-
-print(encode)
-print(decode(encode2))
-print(decode(encode3))
-print(decode(encode4))
-print(decode(encode5))
-print(decode(encode6))
+# for the test suite see related file in folder
