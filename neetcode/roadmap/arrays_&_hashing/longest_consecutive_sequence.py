bace0f89c83d45e6ad5c9a700b56398226157a87
Andrea Canestrari <andrea_4991@hotmail.it>
2026-05-08 13:33:36 +0100

    Perfected longest consecutive sequence solution, this one was easier than expected

--- neetcode/roadmap/arrays_&_hashing/longest_consecutive_sequence.py
+++ neetcode/roadmap/arrays_&_hashing/longest_consecutive_sequence.py
@@ -23,20 +23,27 @@ Constraints:
 
 class Solution:
     def longestConsecutive(self, nums: list[int]) -> int:
+        if not nums:
+            return 0
+        
         num_sequences = dict()
 
         for num in sorted(nums):
             if (num - 1) in num_sequences:
-                num_sequences[num-1].append(num)
-                num_sequences[num] = num_sequences[num-1]
+                if num in num_sequences[num-1]:
+                    continue
+                else:
+                    num_sequences[num-1].append(num)
+                    num_sequences[num] = num_sequences[num-1]
             else:
                 num_sequences[num] = [num]
 
-        index, sequence_len = max(num_sequences.items(), key=lambda item: len(item[1]))
+        index, max_sequence = max(num_sequences.items(), key=lambda item: len(item[1]))
 
-        return max(sequence_len)
+        return len(max_sequence)
     
 
-print(Solution().longestConsecutive([1,2,3,5]))
+nums = [2,20,4,10,3,4,5]
+print(Solution().longestConsecutive(nums))
     
 
