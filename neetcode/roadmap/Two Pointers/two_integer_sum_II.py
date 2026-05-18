2fb55ff375fa9b07615993e6c7f21960c19ff9da
Andrea Canestrari <andrea_4991@hotmail.it>
2026-05-18 20:04:06 +0100

    First attempt solution using 2 pointers opposite direction, different speed

--- neetcode/roadmap/Two Pointers/two_integer_sum_II.py
+++ neetcode/roadmap/Two Pointers/two_integer_sum_II.py
@@ -17,3 +17,26 @@ The sum of 1 and 2 is 3. Since we are assuming a 1-indexed array, index1 = 1, in
 
 https://neetcode.io/problems/two-integer-sum-ii/question?list=neetcode150
 '''
+
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        # numbers is sorted in increasing order (non decreasing).
+        index1, index2= 0, len(numbers) - 1
+
+        while index1 < index2:
+            # ascending order, therefore num_right = small, num_left = big
+            num_right, num_left = numbers[index1], numbers[index2]
+            temp_sum = num_right + num_left
+
+            if temp_sum == target:
+                # indices must be 1-indexed.
+                return [index1 + 1, index2 + 1]
+            # sum too big, reduce num_left
+            elif temp_sum > target:
+                index2 -= 1
+            # sum too small, increase num_right
+            else:
+                index1 += 1
+        # placeholder return, the exercise guarantees one valid solution per test case
+        return [0,0]
+        
