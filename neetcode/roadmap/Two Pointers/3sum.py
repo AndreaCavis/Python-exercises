91161f27b7431d1ed06baf2d39537f1d83172d1c
Andrea Canestrari <andrea_4991@hotmail.it>
2026-05-20 13:21:16 +0100

    Warmup. Recoded latest neetcode exercise

--- neetcode/roadmap/Two Pointers/3sum.py
+++ neetcode/roadmap/Two Pointers/3sum.py
@@ -2,12 +2,15 @@
 
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
+        # sort the list to operate with 2 pointers algorithm
         sorted_nums = sorted(nums)
         n = len(sorted_nums)
         result_set = set()
 
-        for j in range(1, n-1):
-            i, k = 0, len(sorted_nums) -1
+        for j in range(1, n - 1):
+            i, k = 0, n - 1
+
+            # [-4, -1, -1, 0, 1, 2]
 
             while i < j and j < k:
                 temp_sum = sorted_nums[i] + sorted_nums[j] + sorted_nums[k]
@@ -17,11 +20,9 @@ class Solution:
                     k -= 1
                 elif temp_sum == 0:
                     result_set.add((sorted_nums[i], sorted_nums[j], sorted_nums[k]))
-                    # test: k-=1, i+=1 or k-=1, i+=1 
-                    # PASSED: k-=1, i+=1, AND k-=1
-                    k-=1
-        
-        # DATA FORMATTING (for exercise purpouse)
+                    k -= 1
+                
+        # DATA formatting
         temp_res = [x for x in result_set]
         res = []
         for triplet in temp_res:
