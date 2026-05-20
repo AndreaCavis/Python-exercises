a13b2ad3ded9257413debcabaa3223497b4e3d3a
Andrea Canestrari <andrea_4991@hotmail.it>
2026-05-20 14:11:11 +0100

    Initial draft of solution

--- neetcode/roadmap/Two Pointers/container_with_most_water.py
+++ neetcode/roadmap/Two Pointers/container_with_most_water.py
@@ -19,6 +19,23 @@ Output: 4
 https://neetcode.io/problems/max-water-container/question?list=neetcode150
 '''
 
+# Ok, so the task asks me the max_area I can produce with n bars where n is the len(heights)
+# min(height[l], height[r]) is the height of the bar (1st dimension) and 
+# the distance between the indices 'l' and 'r' (2nd dimension) multiplied will produce curr_area.
+# I need to evaluate each area I can form and select the max(max_area, curr_area)
+
 class Solution:
     def maxArea(self, heights: list[int]) -> int:
+        n = len(heights) - 1
+        l, r = 0, n
+        max_area = float("-inf")
+
+        while l < r:
+            height = min(height[l], height[r])
+            length = r - l
+            curr_area = height * length
+            max_area = max(curr_area, max_area)
+
+
+
         return 0
