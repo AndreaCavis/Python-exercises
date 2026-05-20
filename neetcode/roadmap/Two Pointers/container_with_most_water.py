322ef7fbf45234e96059f39489f20a70234b350d
Andrea Canestrari <andrea_4991@hotmail.it>
2026-05-20 13:51:52 +0100

    Starting problem 'Container with most water', this is gonna be challenging

---
+++ neetcode/roadmap/Two Pointers/container_with_most_water.py
@@ -0,0 +1,5 @@
+# I already know this problem is thought, probably have to inform myself about it. 
+# I remember seeing the execution of this algorithm on python alchemist. Yeah, here it is:
+# https://www.pythonalchemist.com/blog/trapping-rain-water
+#  ok, let's dig it
+
