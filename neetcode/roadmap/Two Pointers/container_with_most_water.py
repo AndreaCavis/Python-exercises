f1909df8f0de1cbacda968e2209e8e4fd52f159a
Andrea Canestrari <andrea_4991@hotmail.it>
2026-05-21 12:36:20 +0100

    New day, refactoring. It's not immediate, I need to think about it and do several attempts before getting to the right answer

--- neetcode/roadmap/Two Pointers/container_with_most_water.py
+++ neetcode/roadmap/Two Pointers/container_with_most_water.py
@@ -1,8 +1,3 @@
-# I already know this problem is thought, probably have to inform myself about it. 
-# I remember seeing the execution of this algorithm on python alchemist. Yeah, here it is:
-# https://www.pythonalchemist.com/blog/trapping-rain-water
-#  ok, let's dig it
-
 '''
 You are given an integer array heights where heights[i] represents the height of the i(th) bar.
 You may choose any two bars to form a container. 
