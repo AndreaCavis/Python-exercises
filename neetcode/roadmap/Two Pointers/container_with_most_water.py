aad8c613d774f0a1764a360c08ddcdfea70de978
Andrea Canestrari <andrea_4991@hotmail.it>
2026-05-21 17:15:34 +0100

    Solved problem thanks to hint 3 which confirmed my suspicion on having to move the smaller bar. I should have tried it first, fuck

--- neetcode/roadmap/Two Pointers/container_with_most_water.py
+++ neetcode/roadmap/Two Pointers/container_with_most_water.py
@@ -19,29 +19,33 @@ https://neetcode.io/problems/max-water-container/question?list=neetcode150
 # the distance between the indices 'l' and 'r' (2nd dimension) multiplied will produce curr_area.
 # I need to evaluate each area I can form and select the max(max_area, curr_area)
 
-# NOTE: Naive Solution, needs refactoring
 class Solution:
     def maxArea(self, heights: list[int]) -> int:
         n = len(heights) - 1
         max_area = 0
-        
-        # Brute Force approach. O(n)2
-        for l in range(n):
-            r = n
-            while l < r:
-                height = min(heights[l], heights[r])
-                length = r - l
-                curr_area = height * length if length != 0 else 1
-                max_area = max(curr_area, max_area)
+        l, r = 0, n
+
+        while l < r:
+            height = min(heights[l], heights[r])
+            length = r - l
+            curr_area = height * length
+            max_area = max(curr_area, max_area)
+            # I KNEW THIS WAS THE APPROACH BUT I SECOND GUESSED MYSELF
+            # AND TALKED ME OUT OF IT. I NEED TO FIGURE THIS OUT
+            if heights[l] < heights[r]:
+                l += 1
+            else:
                 r -= 1
 
         return max_area
+
 
 
 height=[1,7,2,5,4,7,3,6]
 height2 = [1,2]
 height3 = [1,7,2,5,12,3,500,500,7,8,4,7,3,6]
 
-# print(Solution().maxArea(height)) # 36
-# print(Solution().maxArea(height2)) # 1
+print(Solution().maxArea(height)) # 36
+print(Solution().maxArea(height2)) # 1
 print(Solution().maxArea(height3)) # 500
+print("Ciao")
