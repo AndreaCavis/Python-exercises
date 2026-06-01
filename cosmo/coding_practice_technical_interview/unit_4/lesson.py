c6a066457a83c37f8db593d3e2dfa3b211b3bada
Andrea Canestrari <andrea_4991@hotmail.it>
2026-06-01 12:05:00 +0100

    First Solution Draft

--- cosmo/coding_practice_technical_interview/unit_4/lesson.py
+++ cosmo/coding_practice_technical_interview/unit_4/lesson.py
@@ -28,16 +28,21 @@ For the input values forest = [0, 1, 0, 0, 0, 0, 1, 1], start = 0, and direction
 def calculate_jump(forest, start, direction):
     n = len(forest) - 1
     valid_solutions = []
-    jump = 1
-    
-    for jump in range(start, n):
-        current_step = forest[start]
-    
-        if current_step == 1:
-            jump += 1
-            return -1
-        else:
-            next_step = current_step + jump
+    # i = start
+    # current_step = forest[start]
+
+    for jump in range(start, n, direction):
+        i = start
+
+        while i < n:
+            current_step = forest[i]
+            if current_step == 1:
+                valid_solutions.append(-1)
+                continue
+
+            i += jump
+            if i > n:
+                valid_solutions.append(jump)
     
     return min(valid_solutions)
 
