b5a3f769b1543280f747bad49b6b2f1b9cb218f2
Andrea Canestrari <andrea_4991@hotmail.it>
2026-06-05 17:54:13 +0100

    Initialised lesson.py

--- cosmo/coding_practice_technical_interview/unit_5/lesson.py
+++ cosmo/coding_practice_technical_interview/unit_5/lesson.py
@@ -1,3 +1,26 @@
 '''
+Picture a quaint, small town where every house is numbered sequentially from 1 to n. One day, a festive town event is held, and balloons are tied to each house.
+The festivities do not end there. At the conclusion of the event, a fun game is played:
+at each step of the game, each house sends half of its balloons to the neighboring house simultaneously 
+(the neighbor on the right side, and for the last house, the neighbor is the first house). 
+The game goes on until at some step there are no changes in the amount of balloons compared to the previous step.
 
+The task is to create a Python function, solution(balloons), where balloons is a list representing the number of balloons at each house. 
+The function should simulate this game and return the number of steps in the game.
+
+For example, if balloons = [4, 1, 2], the output should be solution(balloons) = 3.
+- After the first step, the list becomes [3, 3, 1]. 
+  This is because the first house sends 2 balloons and gets 1, the second house sends nothing but gets 2, and the third house sends 1 but receives nothing.
+Note that when the number of balloons x is odd, than the house sends (x - 1) / 2 balloons. 
+- After the second step, the list becomes [2, 3, 2] and never changes after that.
+- So after the third step, the process finishes.
 '''
+
+
+def solution(balloons):
+    n = len(balloons)
+    pass
+
+
+balloons = [4, 1, 2]
+print(solution(balloons)) # 3
