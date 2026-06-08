c923ab175dc18ace25ec8ef8f43e56f834e3c08c
Andrea Canestrari <andrea_4991@hotmail.it>
2026-06-08 15:06:24 +0100

    First attemp task2/unit5

--- cosmo/coding_practice_technical_interview/unit_5/task2.py
+++ cosmo/coding_practice_technical_interview/unit_5/task2.py
@@ -3,10 +3,12 @@ Imagine a medieval tournament where knights participate in jousting matches.
 The knights are arranged in a circular formation (represented as an array in your program),
 and each knight is initially assigned strength, represented as integers from 1 to 100, determined randomly.
 
-The game consists of rounds. On each round, each knight fights the knight on his right side by subtracting the strength of his opponent from his own.
+The game consists of rounds. On each round, each knight fights the knight on his right side
+by subtracting the strength of his opponent from his own.
 Since this is a circular game, the knight on the right side of the last knight in the array is the first knight.
 Note that all matches are played in parallel, so the strengths are updated only after all matches are played. 
-If after a match, a knight's strength becomes equal to or less than zero, symbolizing the knight's defeat, the knight is removed from the game in the next round.
+If after a match, a knight's strength becomes equal to or less than zero, symbolizing the knight's defeat, 
+the knight is removed from the game in the next round.
 
 The game continues until a situation develops in which no more moves can be made. 
 This happens either when there is just one knight standing or all remaining knights have equal strength meaning no knight can win a match.
@@ -16,6 +18,22 @@ Given the list of knights' strengths in the initial order, your program should c
 
 
 def tournament(knights: list[int]) -> int:
+    n = len(knights)
     rounds = 0
+
+    while True:
+        rounds += 1
+        new_knights = knights.copy()
+
+        if not new_knights:
+            break
+
+        for i in range(n):
+            damage = knights[i] - knights[(i+1)%n]
+            if knights[i] - damage > 0:
+                new_knights.append(knights[i])
+            
+        knights = new_knights
+
     return rounds
 
