cc9a107c88017365c0bdfe73395e536cdb4e0007
Andrea Canestrari <andrea_4991@hotmail.it>
2026-06-10 17:01:49 +0100

    WIP t3/u5. I have to figure out how to traverse numbers like strings or if I should stringigy every result

--- cosmo/coding_practice_technical_interview/unit_5/task3.py
+++ cosmo/coding_practice_technical_interview/unit_5/task3.py
@@ -62,9 +62,22 @@ This sequence of transformations leads to the final set of house numbers, [362, 
 def house_game(houses: list[int]) -> list[int]:
     n = len(houses)
 
-    new_houses = []
+    max_num = max(houses)
+
+    # m = len([x for x in max_num])
+
 
     while True:
+        new_houses = []
+
+        # Addendum: n * 10^2 puts n in the hundrends, 10^3 in the thousands, etc. Dunno if it's needed here
+        # for i in range()
+        for i in range(n):
+            digit = houses[i] // abs(0 - 1 - i)
+            new_houses
+            # implement logic
+            print(i)
+        
         if new_houses == houses:
             break
 
