de177699b9c4192dbccaba363f28101ae43b620d
Andrea Canestrari <andrea_4991@hotmail.it>
2026-06-14 14:12:43 +0100

    updated test suite

--- cosmo/Mastering Algorithms and Data Structures in Python/hashing_dictionaries_and_sets_in_python/unit_3/lesson.py
+++ cosmo/Mastering Algorithms and Data Structures in Python/hashing_dictionaries_and_sets_in_python/unit_3/lesson.py
@@ -4,5 +4,7 @@ def array_intersection(list1: list, list2: list) -> list :
     intersection = set(list1) & set(list2)
     return sorted(list(intersection))
 
+# output: [0,20,40,60,80]
+print(array_intersection([x for x in range(0,100,10) if x % 20 == 0], [x*10 for x in range(10)]))
 
 '''Problem 2: Non-Repeating Elements'''
