c86602f3968fd2ecc4cb18c6d484a52e99b5ac29
Andrea Canestrari <andrea_4991@hotmail.it>
2026-06-14 14:08:13 +0100

    test suite

--- cosmo/Mastering Algorithms and Data Structures in Python/hashing_dictionaries_and_sets_in_python/unit_3/lesson.py
+++ cosmo/Mastering Algorithms and Data Structures in Python/hashing_dictionaries_and_sets_in_python/unit_3/lesson.py
@@ -11,5 +11,7 @@ print(array_intersection([1,2,3], [60,20,3,4,7,2]))
 # output: [1,5,9]
 print(array_intersection([x for x in range(10)], [1,20,5,9,11]))
 
+# output: [10,20,30]
+print(array_intersection([x for x in range(0,100,10)], [10,20,30,456,7,8,99]))
 
 '''Problem 2: Non-Repeating Elements'''
