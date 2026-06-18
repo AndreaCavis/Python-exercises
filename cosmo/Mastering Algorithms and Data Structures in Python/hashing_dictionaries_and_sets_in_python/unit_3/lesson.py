3f4833588b4957ad9f09e305b8590f9e3ed5114c
Andrea Canestrari <andrea_4991@hotmail.it>
2026-06-18 13:45:31 +0100

    Added slightly more context to lesson to be more useful in future

--- cosmo/Mastering Algorithms and Data Structures in Python/hashing_dictionaries_and_sets_in_python/unit_3/lesson.py
+++ cosmo/Mastering Algorithms and Data Structures in Python/hashing_dictionaries_and_sets_in_python/unit_3/lesson.py
@@ -1,4 +1,11 @@
-''' Problem 1: Array Intersection '''
+''' Problem 1: Array Intersection
+
+Our journey begins with the challenge of identifying the intersection of two arrays. 
+In other words, we aim to pinpoint the elements that appear in both of the given lists. 
+It's important to note that we're interested in locating unique common elements
+- even if an element appears more than once in both lists,
+it should only feature once in our output.
+'''
 
 def array_intersection(list1: list, list2: list) -> list :
     set1, set2 = set(list1), set(list2)
@@ -14,7 +21,11 @@ print(array_intersection([x for x in range(0,100,10) if x % 20 == 0], [x*10 for 
 
 
 
-''' Problem 2: Non-Repeating Elements '''
+''' Problem 2: Non-Repeating Elements 
+
+Our next issue is slightly more complex. We must determine all elements in a given list that appear only once,
+meaning they don't have any duplicates in the same list.
+'''
 
 def non_repeating_elements(nums: list) -> list:
     seen, repeated = set(), set()
@@ -29,7 +40,14 @@ def non_repeating_elements(nums: list) -> list:
 
 
 
-''' Problem 3: Unique Elements '''
+''' Problem 3: Unique Elements 
+
+The third problem compels us to find elements unique to each of the two given lists, i.e. given two lists, list1 and list2, 
+we need to find elements that exist only in list1 and elements that exist only in list2, respectively.
+
+Such a task might be beneficial if you possess two lists of employees from different company departments
+and you wish to identify the employees unique to each department.
+'''
 
 def unique_elements(list1: list, list2: list) -> tuple[list, list]:
     set1, set2 = set(list1), set(list2)
