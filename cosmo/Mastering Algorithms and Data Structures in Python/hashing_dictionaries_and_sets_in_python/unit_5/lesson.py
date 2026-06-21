33127720afb28d84f5aede30c3da3eb7722286e9
Andrea Canestrari <andrea_4991@hotmail.it>
2026-06-21 14:29:03 +0100

    Lesson on hash tables 2/3

--- cosmo/Mastering Algorithms and Data Structures in Python/hashing_dictionaries_and_sets_in_python/unit_5/lesson.py
+++ cosmo/Mastering Algorithms and Data Structures in Python/hashing_dictionaries_and_sets_in_python/unit_5/lesson.py
@@ -32,5 +32,39 @@ Under the hood, the Python interpreter uses a hash function to assign each key-v
 
 
 
+'''Collision Handling in Hash Tables
+
+There are instances when two different keys produce the same index after being processed through the hash function. 
+This situation is known as a collision. When a collision occurs, 
+we are faced with a dilemma - where do we store the new key-value pair since that index is already occupied?
+
+Here are two common strategies to handle such scenarios:
+
+1. Chaining: 
+    In this method, each index (or bucket) in the array hosts a linked list of all key-value pairs that hash to the same index. 
+    When a collision occurs, we simply go to the collided index and append the new key-value pair to the existing linked list.
+
+2. Open Addressing: 
+    Upon encountering a collision, the hash table searches for another free slot or index in the table (possibly the next available empty slot)
+    and assigns that location to the new key-value pair. This approach requires a suitable probing strategy to ensure efficient use of table space. 
+    
+The image below provides a visual example of Chaining collision resolving method (// you'll have to imagine this one) 
+John Smith and Sandra Dee have the same hash function result, so their entries are organized in a linked list in the corresponding bucket.
+
+(basically 5 names are stored in different buckets, each index marked with an integer. Both John Smith and Sandra Dee share the index 152.
+Therefore, when accessing 152, you'll find a linked list with both values, at least in this image example)
 '''
-'''
+
+
+'''Time and Space Complexity Analysis for Hash Tables
+
+Hash tables are renowned for their efficiency and speed when it comes to data storage and retrieval. 
+They boast constant time complexity O(1) for the operations on key-value pairs - insertion, deletion, and retrieval. 
+This efficiency comes from a good hash function, which allows for keys to be uniformly distributed across the table 
+and accessed directly via their indices, eliminating the need to scan through unnecessary slots.
+
+Although hash tables generally perform robustly, situations may arise where frequent collisions occur. 
+Such situations could deteriorate the table's efficiency and extend the time complexity to a worst-case scenario of O(n), 
+where n is the number of keys hashing to the same index. '''
+
+
